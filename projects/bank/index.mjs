b38f2c300dcb8be79afad6d7897678dfb8cccec4
Gabriel Ruiz <51479498+GobsRuiz@users.noreply.github.com>
2022-04-06 18:11:36 -0300

    Project bank

--- projects/bank/index.mjs
+++ projects/bank/index.mjs
@@ -31,11 +31,17 @@ function operation(){
       case 'Criar conta':
         createAccount()
         break;
+      case 'Consultar saldo':
+        getAccountBalance()
+        break;
       case 'Depositar':
         deposit()
         break;
+      case "Sacar":
+        withdraw()
+        break;
       default:
-        console.log(chalk.rgb(10, 255, 10)("Obrigado por usar o bank!"))
+        console.log(chalk.rgb(10, 80, 255)("Obrigado por usar o bank!"))
         process.exit()
     }
   })
@@ -47,71 +53,150 @@ operation()
 function createAccount(){
   console.log(chalk.rgb(10, 255, 10)("Obrigado por escolher nosso banco. \nVamos dar início a criação da sua conta."))
 
-  buildAccount()
+  // Ask for account name
+  prompt([
+    {
+      name: "accountName",
+      message: "Digite um nome para sua conta:"
+    }
+  ])
+  .then((answers) => {
+    const accountName = answers['accountName'].split(" ").join("_"); 
 
-  // Build account
-  function buildAccount(){
-    prompt([
-      {
-        name: "accoutName",
-        message: "Digite um nome para sua conta:"
-      }
-    ])
-    .then((answers) => {
-      const accountName = answers['accountName']; 
-      
-      // Accounts folder exists?
-      if(!fs.existsSync('accounts')) fs.mkdirSync('accounts');
-      
-      // Account name exists?
-      if(fs.existsSync(`accounts/${accountName}.json`)) {
-        console.log(chalk.bgRgb(255, 0, 0).rgb(0, 0, 0).bold("Esta conta já existe!"))
-        
-        // Restart
-        return buildAccount();
-      }
-  
-      // Create archive
-      fs.writeFileSync(
-        `accounts/${accountName}.json`,
-        '{"balance": 0}',
-  
-        (error) => console.log(error)
-      )
-  
-      // Success message
-      console.log(chalk.bgRgb(10, 255, 10).rgb(0, 0, 0).bold("Parabéns! Sua conta foi criada!"))
-    })
-    .catch((error) => console.log(error))
-  }
+    // Check if the user typed something
+    if(accountName == "") return buildAccount()
+    
+    // Accounts folder exists?
+    if(!fs.existsSync('accounts')) fs.mkdirSync('accounts');
+    
+    // Account name exists?
+    if(fs.existsSync(`accounts/${accountName}.json`)) {
+      console.log(chalk.bgRgb(255, 0, 0).rgb(0, 0, 0).bold("Esta conta já existe!"))
+
+      // Restart
+      return buildAccount();
+    }
+
+    // Create archive
+    fs.writeFileSync(
+      `accounts/${accountName}.json`,
+      '{"balance": 0}',
+
+      (error) => console.log(error)
+    )
+
+    // Success message
+    console.log(chalk.bgRgb(10, 255, 10).rgb(0, 0, 0).bold("Parabéns! Sua conta foi criada!"))
+    
+    setTimeout(() => operation(), 1500)
+  })
+  .catch((error) => console.log(error))
 }
 
 // Deposit
 async function deposit(){
-  function accountName(){
-    prompt([
-      {
-        name: "accountName",
-        message: "Qual o nome da sua conta",
-      },
-    ])
-    .then((answers) => {
-      const accountName = answers['accountName']
-  
-      if(!checkIfTheAccountExists(accountName)){
-        return false;
-      }else{
-        return true
-      }
-    })
-    .catch((error) => console.log(error))
-  }
+  let amountToDeposit;
+  const accountName = await askForAccountName()
+  if(!accountName) return deposit()
 
-  await accountName()
-  console.log("ioi")
+  const accountData = await getAccount(accountName)
+  if(!accountData) return deposit()
+
+  // What amount to deposit
+  await prompt([
+    {
+      name: "amountToDeposit",
+      message: "Quanto você quer depositar?",
+    },
+  ])
+  .then((answers) => {
+    amountToDeposit = answers['amountToDeposit'];
+  })
+
+  if(!amountToDeposit) return deposit()
+
+  // Calculate
+  accountData.balance = parseFloat(accountData.balance) + parseFloat(value);
+
+  // Save file
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    (error) => console.log(error)
+  )
+
+  console.log(chalk.bgRgb(10, 255, 10).rgb(0, 0, 0).bold(`Sucesso! Foi depositado o valor de $${value} na conta ${accountName.split("_").join(" ")}.`))
 }
 
-// Verifications
+// Show balance
+async function getAccountBalance(){
+  const accountName = await askForAccountName();
+  if(!accountName) return getAccountBalance()
+
+  const accountData = await getAccount(accountName)
+  if(!accountData) return getAccountBalance();
+
+  console.log(chalk.bgRgb(10, 80, 255).white.bold(`O seu saldo é de $${accountData.balance}`))
+  
+  operation()
+}
+
+// Withdraw money
+async function withdraw(){
+  let withDrawAmount;
+  const accountName = await askForAccountName()
+  if(!accountName) return withdraw()
+
+  const accountData = await getAccount(accountName)
+  if(!accountData) return withdraw()
+
+  // Ask how much you want to withdraw
+  prompt([
+    {
+      name: "howMuchToWithdraw",
+      message: "Quanto você quer sacar?"
+    },
+  ]).then((answers) => {
+    withDrawAmount = answers['howMuchToWithdraw'];
+    
+  }).catch((error) => console.log(error))
+
+  accountData.balance = parseFloat(accountData.balance) - parseFloat(withDrawAmount);
+
+  // Save file
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    (error) => console.log(error)
+  )
+
+  console.log(chalk.bgRgb(10, 255, 10).rgb(0, 0, 0).bold(`Você sacou um valor de $${withDrawAmount} na conta ${accountName.split("_").join(" ")}.`))
+}
+
+
+
+// Reusable functions
+async function askForAccountName(){
+  let result = await prompt([
+    {
+      name: "accountName",
+      message: "Digite o nome da sua conta:"
+    }
+  ])
+  .then((answers) => {
+    const accountName = answers['accountName'].split(" ").join("_"); 
+    
+    if(!checkIfTheAccountExists(accountName)){
+      return false;
+    }
+
+    return accountName;
+  })
+  .catch((error) => console.log(error))
+
+  return result;
+}
+
 function checkIfTheAccountExists(accountName){
   if(!fs.existsSync(`accounts/${accountName}.json`)){
     console.log(chalk.bgRgb(255, 0, 0).rgb(0, 0, 0).bold("Essa conta não existe, tente novamente!"))
@@ -119,3 +204,13 @@ function checkIfTheAccountExists(accountName){
   }
   return true;
 }
+
+async function getAccount(accountName){
+  const account = fs.readFileSync(`accounts/${accountName}.json`, {
+    encoding: "utf-8",
+    flag: "r"
+  })
+
+  return JSON.parse(account)
+}
+
